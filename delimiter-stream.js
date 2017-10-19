933f0797404603e2751bdd302f4ccb9d6b87ec5f
GitHub <noreply@github.com>
2017-10-19 10:24:09 +0200

    [DEV]: Changed the module so that the delimiter can contain multiple characters. Fixed an issue where the encoding was not considered in all cases where it should be applied. Fixed a rare issue, where multi-byte limiter characters were not parsed properly due to incomplete transmission between 'data' events. Refactored the code base to meet ES6 recommendations.

--- delimiter-stream.js
+++ delimiter-stream.js
@@ -1,54 +1,67 @@
-var Transform = require('stream').Transform;
-var util = require('util');
+'use strict';
 
+const Transform = require('stream').Transform;
+const util = require('util');
+const defaults = {
+  delimiter: '\n',
+  encoding: 'utf8'
+};
 
 util.inherits(DelimiterStream, Transform);
 
-function DelimiterStream(options) {
+function DelimiterStream (options) {
   Transform.call(this, options);
-
-  var defaults = {
-    delimiter: '\n',
-    encoding: 'utf8'
-  };
 
   options = options || {};
   this._delimiter = options.delimiter ? options.delimiter : defaults.delimiter;
   this._encoding = options.encoding ? options.encoding : defaults.encoding;
-  this._stub = '';
-};
+  this._stub = Buffer.from('', this._encoding);
+}
 
-DelimiterStream.prototype._transform = function(chunk, encoding, done) {
-  var self = this;
-  var s = chunk.toString(this._encoding);
+DelimiterStream.prototype._transform = function (chunk, chunkEncoding, done) {
+  const self = this;
+  const encoding = this._encoding;
+  const delimiter = this._delimiter;
+  const chunkText = chunk.toString(encoding);
+  const lines = chunkText.split(delimiter);
 
-  var lines = s.split(this._delimiter);
   if (lines.length > 1) {
-    lines.forEach(function(l, index) {
+    lines.forEach(function (line, index) {
+      // In case it's the first line combine with the previous stub
       if (index === 0) {
-        // First part, append it to the stub and push it
-        self.push(self._stub + l);
-        self._stub = '';
+        self._stub = Buffer.concat([self._stub, Buffer.from(line, encoding)]);
+
+        const chunk = self._stub.toString(encoding);
+
+        if (chunk.indexOf(delimiter) === -1) {
+          self.push(chunk, encoding);
+        } else {
+          chunk
+              .split(delimiter)
+              .forEach((subChunk) => self.push(subChunk, encoding));
+        }
+
+        self._stub = Buffer.from('', encoding);
       } else if (index === lines.length - 1) {
         // Last part of the chunk, this will be the new stub and the beginning
         // of the next chunk (until delimiter) will be appended to this
-        self._stub = l;
+        self._stub = Buffer.from(line, encoding);
       } else {
         // This must be a part cleanly separated by the delimiter within the
         // same chunk, push it
-        self.push(l);
+        self.push(line, encoding);
       }
     });
   } else {
     // No delimiter found, append the chunk to the stub
-    this._stub = this._stub + lines[0];
+    this._stub = Buffer.concat([this._stub, Buffer.from(lines[0], encoding)]);
   }
 
   done();
 };
 
-DelimiterStream.prototype._flush = function(done) {
-  this.push(this._stub);
+DelimiterStream.prototype._flush = function (done) {
+  this.push(this._stub.toString(this._encoding), this._encoding);
   done();
 };
 
--- test/test.js
+++ test/test.js
@@ -1,69 +1,70 @@
-var fs = require('fs');
-var path = require('path');
-var Readable = require('stream').Readable;
-var Writable = require('stream').Writable;
-var StringDecoder = require('string_decoder').StringDecoder;
-var _ = require('lodash');
-var expect = require('chai').expect;
+'use strict';
 
-var DelimiterStream = require('../delimiter-stream');
+const fs = require('fs');
+const path = require('path');
+const Readable = require('stream').Readable;
+const Writable = require('stream').Writable;
+const StringDecoder = require('string_decoder').StringDecoder;
+const _ = require('lodash');
+const expect = require('chai').expect;
 
+const DelimiterStream = require('../delimiter-stream');
 
-describe('delimiter-stream', function() {
+describe('delimiter-stream', function () {
 
-  beforeEach(function() {
+  beforeEach(function () {
     this.decoder = new StringDecoder('utf8');
   });
 
-  it('should use linebreak as default delimiter', function() {
-    var linestream = new DelimiterStream();
+  it('should use linebreak as default delimiter', function () {
+    const linestream = new DelimiterStream();
     expect(linestream._delimiter).to.equal('\n');
   });
 
-  it('should use utf8 as default encoding', function() {
-    var linestream = new DelimiterStream();
+  it('should use utf8 as default encoding', function () {
+    const linestream = new DelimiterStream();
     expect(linestream._encoding).to.equal('utf8');
   });
 
-  it('should support piping', function(done) {
-    var self = this;
-    var input = createReadStream('first|second|third|');
+  it('should support piping', function (done) {
+    const self = this;
+    const input = createReadStream('first|second|third|');
 
-    var delimiterstream = new DelimiterStream({
+    const delimiterstream = new DelimiterStream({
       delimiter: '|'
     });
 
-    var expectedOutcome = [
+    const expectedOutcome = [
       'first',
       'second',
       'third'
     ];
-    var output = new Writable();
-    output._write = function(chunk, encoding, callback) {
-      var actual = self.decoder.write(chunk);
-      var expected = expectedOutcome.shift();
+    const output = new Writable();
+    output._write = function (chunk, encoding, callback) {
+      const actual = self.decoder.write(chunk);
+      const expected = expectedOutcome.shift();
       expect(actual).to.equal(expected);
       callback();
     };
 
-    output.on('finish', function() {
+    output.on('finish', function () {
       expect(expectedOutcome).to.have.length(0);
       done();
     });
 
     input
-      .pipe(delimiterstream)
-      .pipe(output);
+        .pipe(delimiterstream)
+        .pipe(output);
   });
 
-  it('should support data event', function(done) {
-    var self = this;
+  it('should support data event', function (done) {
+    const self = this;
 
-    var delimiterstream = new DelimiterStream({
+    const delimiterstream = new DelimiterStream({
       delimiter: '|'
     });
 
-    var expectedOutcome = [
+    const expectedOutcome = [
       'first',
       'second',
       'third',
@@ -72,13 +73,13 @@ describe('delimiter-stream', function() {
       'sixth'
     ];
 
-    delimiterstream.on('data', function(chunk) {
-      var actual = self.decoder.write(chunk);
-      var expected = expectedOutcome.shift();
+    delimiterstream.on('data', function (chunk) {
+      const actual = self.decoder.write(chunk);
+      const expected = expectedOutcome.shift();
       expect(actual).to.equal(expected);
     });
 
-    delimiterstream.on('finish', function() {
+    delimiterstream.on('finish', function () {
       expect(expectedOutcome).to.have.length(0);
       done();
     });
@@ -91,24 +92,24 @@ describe('delimiter-stream', function() {
     delimiterstream.end();
   });
 
-  it('should ignore empty lines', function(done) {
-    var self = this;
+  it('should ignore empty lines', function (done) {
+    const self = this;
 
-    var delimiterstream = new DelimiterStream();
+    const delimiterstream = new DelimiterStream();
 
-    var expectedOutcome = [
+    const expectedOutcome = [
       'first',
       'second',
       'third'
     ];
 
-    delimiterstream.on('data', function(chunk) {
-      var actual = self.decoder.write(chunk);
-      var expected = expectedOutcome.shift();
+    delimiterstream.on('data', function (chunk) {
+      const actual = self.decoder.write(chunk);
+      const expected = expectedOutcome.shift();
       expect(actual).to.equal(expected);
     });
 
-    delimiterstream.on('finish', function() {
+    delimiterstream.on('finish', function () {
       expect(expectedOutcome).to.have.length(0);
       done();
     });
@@ -117,50 +118,60 @@ describe('delimiter-stream', function() {
     delimiterstream.end();
   });
 
-  it('should support streaming huge files', function(done) {
-    this.timeout(30000);
-    var self = this;
-    var filename = createHugeFile();
+  it('should support streaming huge files', function (done) {
+    const self = this;
+    const encoding = 'utf8';
+    const delimiter = '▌▌▌▌';
+    const filename = createHugeFile(delimiter, encoding);
+    const linestream = new DelimiterStream({delimiter, encoding});
 
-    var input = fs.createReadStream(filename);
-    var linestream = new DelimiterStream();
-    var output = new Writable();
-    var i = 1;
-    output._write = function(chunk, encoding, callback) {
-      var actual = self.decoder.write(chunk);
-      var expected = _.padRight('', i, i++);
+    const input = fs.createReadStream(filename);
+    const output = new Writable();
+    let index = 1;
+
+    output._write = function (chunk, encoding, callback) {
+      const actual = self.decoder.write(chunk);
+      const expected = _.padRight('', index, index++);
       expect(actual).to.equal(expected);
       callback();
     };
 
-    output.on('finish', function() {
+    output.on('finish', function () {
       done();
     });
 
     input
-      .pipe(linestream)
-      .pipe(output);
+        .pipe(linestream)
+        .pipe(output);
   });
 });
 
-function createReadStream(text) {
-  var rs = new Readable();
+function createReadStream (text) {
+  const rs = new Readable();
+
   rs.push(text);
   rs.push(null);
+
   return rs;
 }
 
-function createHugeFile() {
-  var filename = path.join(__dirname, 'tmp.txt');
-  if (fs.existsSync(filename)) {
-    fs.unlinkSync(filename);
-  }
-  // Create huge temporary file
-  var delimiter = '\n';
-  for (var i = 1; i < 15000; i++) {
-    var text = _.padRight('', i, i);
-    fs.appendFileSync(filename, text + delimiter);
+function createHugeFile (delimiter, encoding) {
+  const filename = path.join(__dirname, 'tmp.txt');
+
+  removeHugeFile(filename);
+
+  for (let i = 1; i < 5000; i++) {
+    const text = _.padRight('', i, i);
+    fs.appendFileSync(filename, text + delimiter, {encoding});
   }
 
   return filename;
+}
+
+function removeHugeFile (passedFilename) {
+  const filename = passedFilename || path.join(__dirname, 'tmp.txt');
+
+  if (fs.existsSync(filename)) {
+    fs.unlinkSync(filename);
+  }
 }
